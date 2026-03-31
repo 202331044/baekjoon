c90cf3aaa556ee6fe53ded82c28f75c07eecab14
202331044 <131847052+202331044@users.noreply.github.com>
2026-03-31 11:36:20 +0900

    solve: 2743 단어 길이 재기 (step05)

---
+++ 05_string/2743_word_length.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	string str;
+	cin >> str;
+	
+	cout << str.size() << "\n";
+
+	return 0;
+}
