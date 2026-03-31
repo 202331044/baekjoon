8ee7dad1bbe7b7aa691e04f7ae4f0faebd7abe64
202331044 <131847052+202331044@users.noreply.github.com>
2026-03-31 11:48:25 +0900

    solve: 11654 아스키 코드 (step05)

---
+++ 05_string/11654_ascii_code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	char ch;
+	cin >> ch;
+	cout << static_cast<int>(ch) << "\n";
+
+	return 0;
+}
