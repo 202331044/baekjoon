3413e28c2e8c0ec03cc5f9d7c2fe6bf730b8ed50
202331044 <131847052+202331044@users.noreply.github.com>
2026-04-04 16:36:24 +0900

    solve: 2738 2차원 행렬 (step07)

---
+++ 07_2d_array/2738_matrix_addition.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	int n{}, m{};
+	cin >> n >> m;
+
+	vector<vector<int>> a(n, vector<int>(m));
+	//vector<vector<int>> b(n, vector<int>(m));
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+			cin >> a[i][j];
+	}
+
+	//for (int i = 0; i < n; ++i)
+	//{
+	//	for (int j = 0; j < m; ++j)
+	//		cin >> b[i][j];
+	//}
+
+	//for (int i = 0; i < n; ++i)
+	//{
+	//	for (int j = 0; j < m; ++j)
+	//		cout << a[i][j] + b[i][j] << " ";
+	//	cout << "\n";
+	//}
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			int num{};
+			cin >> num;
+			cout << a[i][j] + num << " ";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
