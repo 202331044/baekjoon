9946ee8674679a83cc1bf058dcdc6b38cd3ed990
202331044 <131847052+202331044@users.noreply.github.com>
2026-04-11 16:46:11 +0900

    Create 24266 알고리즘 수업 - 알고리즘의 수행 시간 5 (step11)

---
+++ 11_time_complexity/24266_algorithm_runtime_5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	long long n{};
+	cin >> n;
+
+	cout << n * n * n << "\n";
+	cout << 3 << "\n";
+
+	return 0;
+}
