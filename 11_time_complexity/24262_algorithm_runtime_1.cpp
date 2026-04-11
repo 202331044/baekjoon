70d4cdfd77eb2ca8f2025fad0fe72ca88ca0bebb
202331044 <131847052+202331044@users.noreply.github.com>
2026-04-11 16:16:16 +0900

    solve: 24262 알고리즘 수업 - 알고리즘의 수행 시간 1 (step11)

---
+++ 11_time_complexity/24262_algorithm_runtime_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	int n{};
+	cin >> n;
+
+	cout << 1 << "\n";
+	cout << 0 << "\n";
+
+	return 0;
+}
