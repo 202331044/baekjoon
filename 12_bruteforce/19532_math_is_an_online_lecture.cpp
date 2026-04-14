630f4633228e36cd1eb3eb88e6eedf2d336f3866
202331044 <131847052+202331044@users.noreply.github.com>
2026-04-14 16:24:42 +0900

    feat(19532): add solution using Cramer's rule

--- 12_bruteforce/19532_math_is_an_online_lecture.cpp
+++ 12_bruteforce/19532_math_is_an_online_lecture.cpp
@@ -10,17 +10,22 @@ int main()
 	int a{}, b{}, c{}, d{}, e{}, f{};
 	cin >> a >> b >> c >> d >> e >> f;
 
-	for (int x = -999; x <= 999; ++x)
-	{
-		for (int y = -999; y <= 999; ++y)
-		{
-			if (a * x + b * y == c && d * x + e * y == f)
-			{
-				cout << x << " " << y << "\n";
-				return 0;
-			}
-		}
-	}
+	//for (int x = -999; x <= 999; ++x)
+	//{
+	//	for (int y = -999; y <= 999; ++y)
+	//	{
+	//		if (a * x + b * y == c && d * x + e * y == f)
+	//		{
+	//			cout << x << " " << y << "\n";
+	//			return 0;
+	//		}
+	//	}
+	//}
+
+	int x = (c * e - b * f) / (a * e - b * d);
+	int y = (a * f - c * d) / (a * e - b * d);
+
+	cout << x << " " << y << "\n";
 
 	return 0;
 }
